f06c80eeb27e980f661038c51b5defccddaad34c
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-20 10:59:45 +0000

    Add ArtistWork model, and install rest framework

--- artists/admin.py
+++ artists/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Style, Medium, ArtistWork, Artist
 
+
+admin.site.register(Style)
+admin.site.register(Medium)
+# admin.site.register(ArtistWork)
 admin.site.register(Artist)
--- artists/models.py
+++ artists/models.py
@@ -19,30 +19,40 @@ class Medium(models.Model):
         return self.name
 
 
+# class ArtistWork(models.Model):
+#     # A model for artists to show their work
+#     name = models.CharField(max_length=150, null=False, blank=False)
+#     art_image1 = models.ImageField(
+#         upload_to='images/', default='../default_post_vnjiii'
+#     )
+#     art_image2 = models.ImageField(
+#         upload_to='images/', default='../default_post_vnjiii'
+#     )
+#     art_image3 = models.ImageField(
+#         upload_to='images/', default='../default_post_vnjiii'
+#     )
+
+#     def __str__(self):
+#         return self.name
+
+
 class Artist(models.Model):
     # A model for artists to promote themselves on the design hub site
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, null=False, blank=False)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)    
     profile_image = models.ImageField(
         upload_to='images/', default='../default_profile_qpd7ju'
     )
     location = models.CharField(max_length=255, null=False, blank=False)
     influences = models.TextField(null=False, blank=False)
-    art_image1 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
-    )
-    art_image2 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
-    )
-    art_image3 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
-    )
     medium = models.ForeignKey(
         Medium, on_delete=models.CASCADE, null=False, blank=False
     )
     style = models.ForeignKey(
         Style, on_delete=models.CASCADE, null=False, blank=False
     )
+    # artistwork = models.ForeignKey(
+    #     ArtistWork, on_delete=models.CASCADE, null=False, blank=False
+    # )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
