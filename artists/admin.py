bb2ca73f681e0e4a9129e142e27b5aed70023f47
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-23 10:39:41 +0000

    Delete database and migrations folder to rework artwork model

--- artists/admin.py
+++ artists/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Style, Medium, ArtistWork, Artist
+from .models import Style, Medium, Artwork, Artist
 
 
 admin.site.register(Style)
 admin.site.register(Medium)
-# admin.site.register(ArtistWork)
+admin.site.register(ArtWork)
 admin.site.register(Artist)
--- artists/models.py
+++ artists/models.py
@@ -5,7 +5,12 @@ from django.contrib.auth.models import User
 
 class Style(models.Model):
     # A model for artists to select their art style e.g. abstract, art deco, impressionism etc  # noqa
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        default=1
+        )
 
     def __str__(self):
         return self.name
@@ -13,50 +18,115 @@ class Style(models.Model):
 
 class Medium(models.Model):
     # A model for artists to select their art medium e.g. pottery, oil, sculpture etc  # noqa
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        default=1
+        )
 
     def __str__(self):
         return self.name
 
 
-class ArtistWork(models.Model):
+class Artwork(models.Model):
     # A model for artists to show their work
-    name = models.CharField(max_length=150, null=False, blank=False)
-    art_image1 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
+    artist = models.ForeignKey(
+        Artist,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
-    art_image2 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
+    art_image = models.ImageField(
+        upload_to='images/',
+        default='../default_artwork_gko8de'
     )
-    art_image3 = models.ImageField(
-        upload_to='images/', default='../default_post_vnjiii'
-    )
+    art_image_name = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True,
+        verbose_name='Artwork name'
+        )
+    alt_text = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True,
+        verbose_name='Alt text'
+        )
+    actual_date_of_creation = models.DateTimeField(
+        null=True,
+        blank=True,
+        verbose_name='Created on:'
+        )
+    added_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Added on:'
+        )
+    edited_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Edited on:')
+
+    class Meta:
+        verbose_name = 'Artwork'
 
     def __str__(self):
         return self.name
+
+    @property
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return 'static/images/default_profile_picture_kodvuc'
 
 
 class Artist(models.Model):
     # A model for artists to promote themselves on the design hub site
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)    
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+        )    
     profile_image = models.ImageField(
-        upload_to='images/', default='../default_profile_qpd7ju'
+        upload_to='images/',
+        default='../default_profile_qpd7ju'
     )
-    location = models.CharField(max_length=255, null=True, blank=True)
-    influences = models.TextField(null=True, blank=True)
-    medium = models.ForeignKey(
-        Medium, on_delete=models.CASCADE, null=True, blank=True
+    location = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+        )
+    influences = models.TextField(
+        null=True,
+        blank=True
+        )
+    medium = models.OneToMany(
+        Medium,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
-    style = models.ForeignKey(
-        Style, on_delete=models.CASCADE, null=True, blank=True
+    style = models.OneToMany(
+        Style,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
-    artistwork = models.ForeignKey(
-        ArtistWork, on_delete=models.CASCADE, null=True, blank=True
+    artwork = models.ForeignKey(
+        Artwork,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created on:'
+        )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Edited on:'
+        )
 
     class Meta:
+        verbose_name = 'Artist',
         ordering = ['-created_at']
 
     def __str__(self):
