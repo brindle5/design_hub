85e129a6af002e21438a9d85ce2aa6e7b3201578
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-24 16:45:18 +0000

    Create new database and superuser, add art styles and mediums in admin panel

--- artists/admin.py
+++ artists/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Style, Medium, Artwork, Artist
+from .models import Style, Medium, Artist, Artwork
 
 
 admin.site.register(Style)
 admin.site.register(Medium)
-admin.site.register(ArtWork)
 admin.site.register(Artist)
+admin.site.register(Artwork)
--- artists/models.py
+++ artists/models.py
@@ -28,8 +28,63 @@ class Medium(models.Model):
         return self.name
 
 
+class Artist(models.Model):
+    # A model for artists to promote themselves on the design hub site
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+        )
+    profile_image = CloudinaryField(
+        'profile_image',
+        folder='profile_image',
+        null=True,
+        blank=True,
+    )
+    location = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+        )
+    influences = models.TextField(
+        null=True,
+        blank=True
+        )
+    medium = models.ManyToManyField(
+        Medium
+    )
+    style = models.ManyToManyField(
+        Style
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created on:'
+        )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Edited on:'
+        )
+
+    class Meta:
+        verbose_name = 'Artist',
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+    @property
+    def image_url(self):
+        if self.image:
+            return self.image.url
+        return 'static/images/default_profile_picture.jpg'
+
+
 class Artwork(models.Model):
     # A model for artists to show their work
+    name = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+        )
     artist = models.ForeignKey(
         Artist,
         on_delete=models.CASCADE,
@@ -78,62 +133,6 @@ class Artwork(models.Model):
         if self.image:
             return self.image.url
         return 'static/images/default_artwork.default_artwork.jpg'
-
-
-class Artist(models.Model):
-    # A model for artists to promote themselves on the design hub site
-    owner = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-        )
-    profile_image = CloudinaryField(
-        'profile_image',
-        folder='profile_image',
-        null=True,
-        blank=True,
-    )
-    location = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True
-        )
-    influences = models.TextField(
-        null=True,
-        blank=True
-        )
-    medium = models.ManyToMany(
-        Medium,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True
-    )
-    style = models.ManyToMany(
-        Style,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Created on:'
-        )
-    updated_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Edited on:'
-        )
-
-    class Meta:
-        verbose_name = 'Artist',
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f"{self.owner}'s profile"
-    
-    @property
-    def image_url(self):
-        if self.image:
-            return self.image.url
-        return 'static/images/default_profile_picture.jpg'
 
 
 def create_artist(sender, instance, created, **kwargs):
