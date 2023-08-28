d9e46c72c9ae3446ab5a8a6efcce1fd92f6afa8b
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-28 09:57:09 +0000

    Add artwork detail url to artist urls

--- artists/urls.py
+++ artists/urls.py
@@ -3,7 +3,8 @@ from artists import views
 
 
 urlpatterns = [
-    path('artwork/', views.ArtworkList.as_view()),
     path('artists/', views.ArtistList.as_view()),
     path('artists/<int:pk>', views.ArtistDetail.as_view()),
+    path('artwork/', views.ArtworkList.as_view()),
+    path('artwork/<int:pk>', views.ArtworkDetail.as_view()),
 ]
