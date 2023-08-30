9b1c1ffefb6f305fd669bbd23a9e999f0057ae56
brindle5 <sarah.brindle@hotmail.co.uk>
2023-08-30 16:16:10 +0000

    Add naturadaye to artists and artwor serializersk

--- artists/serializers.py
+++ artists/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from .models import Style, Medium, Artist
+from django.contrib.humanize.templatetags.humanize import naturalday
 
 
 class ArtistSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturalday(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturalday(obj.updated_at)
 
     class Meta:
         model = Artist
--- artwork/serializers.py
+++ artwork/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from .models import Artwork
+from django.contrib.humanize.templatetags.humanize import naturalday
 
 
 class ArtworkSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    added_date = serializers.SerializerMethodField()
+    edited_date = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_added_date(self, obj):
+        return naturalday(obj.added_date)
+
+    def get_edited_date(self, obj):
+        return naturalday(obj.edited_date)
 
     class Meta:
         model = Artwork
