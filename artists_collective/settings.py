3939d9a7845f203d35d43ac221ea9a550f4a5f4c
brindle5 <sarah.brindle@hotmail.co.uk>
2023-09-06 15:36:01 +0000

    Update settings for deployemnt

--- artists_collective/settings.py
+++ artists_collective/settings.py
@@ -64,9 +64,9 @@ SECRET_KEY = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-# DEBUG = 'DEBUG' in os.environ
-DEBUG = False
+
+DEBUG = 'DEBUG' in os.environ
+
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
