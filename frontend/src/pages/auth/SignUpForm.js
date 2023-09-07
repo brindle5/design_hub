ed746a591e7a70dc3cdeb133c597d5c4a027eea1
brindle5 <sarah.brindle@hotmail.co.uk>
2023-09-07 14:06:45 +0000

    Add handle change function to input fields in sign up form

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
 
 // Main layout of form taken from Code Institute's Moments Project
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  })
+
+  const { username, password1, password2 } = signUpData;
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...setSignUpData,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return (
     <Row >
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className= "p-4" >
-          <h3>Sign up for an account</h3>
+          <h3>Sign up for an account!</h3>
 
           <Form>
       <Form.Group className="mb-3" controlId="username">
@@ -18,6 +33,8 @@ const SignUpForm = () => {
             type="text" 
             placeholder="username" 
             name="username" 
+            value={username}
+            onChange={handleChange}
         />
       </Form.Group>
 
@@ -27,6 +44,8 @@ const SignUpForm = () => {
             type="password" 
             placeholder="password" 
             name="password1"
+            value={password1}
+            onChange={handleChange}
         />
       </Form.Group>
 
@@ -36,6 +55,8 @@ const SignUpForm = () => {
             type="password" 
             placeholder="confirm password" 
             name="password2"
+            value={password2}
+            onChange={handleChange}
         />
       </Form.Group>
 
