7e50b976f1c292419ec2c4c350ccea81b44e7006
brindle5 <sarah.brindle@hotmail.co.uk>
2023-09-17 15:18:10 +0000

    Compile static files

--- frontend/src/pages/artwork/ArtworkListPage.js
+++ frontend/src/pages/artwork/ArtworkListPage.js
@@ -44,8 +44,6 @@ function ArtworkListPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-
-      <i className="fas fa-search"/>
         <Form          
           onSubmit={(event) => event.preventDefault()}
         >
